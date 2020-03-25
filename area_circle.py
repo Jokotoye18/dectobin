3a6c00bf538cfd69f91e474956f64edc7d782094
Jokotoye18 <60762524+Jokotoye18@users.noreply.github.com>
2020-03-25 04:51:00 +0100

    Update area_circle.py

--- area_circle.py
+++ area_circle.py
@@ -1,6 +1,18 @@
+# The below code is for Go task 1
+def dectobin(num):
+    num = int(num)
+    print(bin(num))
+    
+dectobin(10)
+
+
+# The below code is for python first task
 def area_circle(radius):
     pi = 3.142
     area = pi * radius**2
     print(area)
 
 area_circle(5)
+
+
+# please pardon my error.
